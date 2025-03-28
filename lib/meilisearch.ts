9e7b1f81194c9df40739840ba1534f65f19c43ea
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-03-28 17:22:18 +0530

    Add environment variable checks for MeiliSearch configuration

--- lib/meilisearch.ts
+++ lib/meilisearch.ts
@@ -1,5 +1,13 @@
 import { MeiliSearch } from 'meilisearch'
 
+if (!process.env.NEXT_PUBLIC_MEILISEARCH_URL) {
+  throw new Error('NEXT_PUBLIC_MEILISEARCH_URL is not defined')
+}
+
+if (!process.env.NEXT_PUBLIC_MEILISEARCH_KEY) {
+  throw new Error('NEXT_PUBLIC_MEILISEARCH_KEY is not defined')
+}
+
 const client = new MeiliSearch({
   host: process.env.NEXT_PUBLIC_MEILISEARCH_URL,
   apiKey: process.env.NEXT_PUBLIC_MEILISEARCH_KEY,
